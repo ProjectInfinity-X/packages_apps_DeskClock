LineageOS/android_packages_apps_DeskClock

DeskClock: Add screensaver display options
This patch addes the following options to the screensaver function:
    * Select color
    * Bold text
    * Hide/show AM/PM
    * Night mode follows DND setting
    * Night mode color
    * Night mode brightness

Change-Id: I4a7edad096721689805ad26265f4c67fb731cd76

src/com/android/deskclock/data/SettingsModel.java
@@ -88,9 +88,28 @@ final class SettingsModel {
         return SettingsDAO.getScreensaverClockStyle(mContext, mPrefs);
     }
 
+    String getScreensaverClockColor() {
+        return SettingsDAO.getScreensaverClockColor(mContext, mPrefs);
+    }
+
+    String getScreensaverClockNightModeColor() {
+        return SettingsDAO.getClockNightModeColor(mContext, mPrefs);
+    }
+
+    boolean getScreensaverNightModeDNDOn() {
+        return SettingsDAO.getScreensaverNightModeDNDOn(mPrefs);
+    }
+
+    int getScreensaverNightModeBrightness() {
+        return SettingsDAO.getScreensaverNightModeBrightness(mPrefs);
+    }
     boolean getScreensaverNightModeOn() {
         return SettingsDAO.getScreensaverNightModeOn(mPrefs);
     }
+
+    boolean getScreensaverShowAmPmOn() { return SettingsDAO.getScreensaverShowAmPmOn(mPrefs); }
+
+    boolean getScreensaverBoldTextOn() { return SettingsDAO.getScreensaverBoldTextOn(mPrefs); }
 
     boolean getShowHomeClock() {
         if (!SettingsDAO.getAutoShowHomeClock(mPrefs)) {

src/com/android/deskclock/settings/ScreensaverSettingsActivity.java
@@ -19,10 +19,11 @@ package com.android.deskclock.settings;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import androidx.preference.CheckBoxPreference;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
+import androidx.preference.SeekBarPreference;
 
 import com.android.deskclock.R;
 import com.android.deskclock.data.DataModel;
@@ -34,7 +35,13 @@ import com.android.deskclock.widget.CollapsingToolbarBaseActivity;
 public final class ScreensaverSettingsActivity extends CollapsingToolbarBaseActivity {
 
     public static final String KEY_CLOCK_STYLE = "screensaver_clock_style";
+    public static final String KEY_CLOCK_COLOR = "screensaver_clock_color";
     public static final String KEY_NIGHT_MODE = "screensaver_night_mode";
+    public static final String KEY_NIGHT_MODE_COLOR = "screensaver_clock_night_mode_color";
+    public static final String KEY_NIGHT_MODE_DND = "screensaver_clock_night_mode_dnd";
+    public static final String KEY_NIGHT_MODE_BRIGHTNESS = "screensaver_clock_night_mode_brightness";
+    public static final String KEY_SHOW_AMPM = "screensaver_show_ampm";
+    public static final String KEY_BOLD_TEXT = "screensaver_bold_text";
     private static final String PREFS_FRAGMENT_TAG = "prefs_fragment";
 
     @Override
@@ -87,19 +94,66 @@ public final class ScreensaverSettingsActivity extends CollapsingToolbarBaseActi
                 final int index = clockStylePref.findIndexOfValue((String) newValue);
                 clockStylePref.setSummary(clockStylePref.getEntries()[index]);
             }
+            if (KEY_CLOCK_COLOR.equals(pref.getKey()) || KEY_NIGHT_MODE_COLOR.equals(pref.getKey())) {
+                final ListPreference clockColorPref = (ListPreference) pref;
+                final int index = clockColorPref.findIndexOfValue((String) newValue);
+                clockColorPref.setSummary(clockColorPref.getEntries()[index]);
+            }
+            if (KEY_NIGHT_MODE_BRIGHTNESS.equals(pref.getKey())) {
+                final SeekBarPreference clockBrightness = (SeekBarPreference) pref;
+                final String progress = String.valueOf(newValue) + "%";
+                clockBrightness.setSummary(progress);
+            }
             return true;
         }
 
         private void refresh() {
             final ListPreference clockStylePref = findPreference(KEY_CLOCK_STYLE);
-            final CheckBoxPreference nightModePref = findPreference(KEY_NIGHT_MODE);
-            if (clockStylePref != null && nightModePref != null) {
+            final ListPreference clockColorPref = findPreference(KEY_CLOCK_COLOR);
+            final ListPreference nightModeColorPref = findPreference(KEY_NIGHT_MODE_COLOR);
+            final SwitchPreferenceCompat nightModePref = findPreference(KEY_NIGHT_MODE);
+            final SwitchPreferenceCompat nightModeDNDPref = findPreference(KEY_NIGHT_MODE_DND);
+            final SwitchPreferenceCompat showAmPmPref = findPreference(KEY_SHOW_AMPM);
+            final SwitchPreferenceCompat boldTextPref = findPreference(KEY_BOLD_TEXT);
+            final SeekBarPreference nightModeBrightness = findPreference(KEY_NIGHT_MODE_BRIGHTNESS);
+            if (clockStylePref != null) {
                 final int index = clockStylePref.findIndexOfValue(DataModel.getDataModel().
                         getScreensaverClockStyle().toString().toLowerCase());
                 clockStylePref.setValueIndex(index);
                 clockStylePref.setSummary(clockStylePref.getEntries()[index]);
                 clockStylePref.setOnPreferenceChangeListener(this);
+            }
+            if (clockColorPref != null) {
+                final int indexColor = clockColorPref.findIndexOfValue(DataModel.getDataModel().
+                        getScreensaverClockColor());
+                clockColorPref.setValueIndex(indexColor);
+                clockColorPref.setSummary(clockColorPref.getEntries()[indexColor]);
+                clockColorPref.setOnPreferenceChangeListener(this);
+            }
+            if (nightModeColorPref != null) {
+                final int indexColor = nightModeColorPref.findIndexOfValue(DataModel.getDataModel().
+                        getScreensaverClockNightModeColor());
+                nightModeColorPref.setValueIndex(indexColor);
+                nightModeColorPref.setSummary(clockColorPref.getEntries()[indexColor]);
+                nightModeColorPref.setOnPreferenceChangeListener(this);
+            }
+            if (nightModePref != null) {
                 nightModePref.setChecked(DataModel.getDataModel().getScreensaverNightModeOn());
+            }
+            if (nightModeDNDPref != null) {
+                nightModeDNDPref.setChecked(DataModel.getDataModel().getScreensaverNightModeDNDOn());
+            }
+            if (showAmPmPref != null) {
+                showAmPmPref.setChecked(DataModel.getDataModel().getScreensaverShowAmPmOn());
+            }
+            if (boldTextPref != null) {
+                boldTextPref.setChecked(DataModel.getDataModel().getScreensaverBoldTextOn());
+            }
+            if (nightModeBrightness != null) {
+                final int percentage = DataModel.getDataModel().getScreensaverNightModeBrightness();
+                nightModeBrightness.setValue(percentage);
+                nightModeBrightness.setSummary(String.valueOf(percentage) + "%");
+                nightModeBrightness.setOnPreferenceChangeListener(this);
             }
         }
     }

ProjectInfinity-X/packages_apps_DeskClock

src/com/android/deskclock/data/SettingsModel.java
@@ -88,8 +88,32 @@ final class SettingsModel {
         return SettingsDAO.getScreensaverClockStyle(mContext, mPrefs);
     }
 
+    String getScreensaverClockColor() {
+        return SettingsDAO.getScreensaverClockColor(mContext, mPrefs);
+    }
+
+    String getScreensaverClockNightModeColor() {
+        return SettingsDAO.getClockNightModeColor(mContext, mPrefs);
+    }
+
+    boolean getScreensaverNightModeDndOn() {
+        return SettingsDAO.getScreensaverNightModeDndOn(mPrefs);
+    }
+
+    int getScreensaverNightModeBrightness() {
+        return SettingsDAO.getScreensaverNightModeBrightness(mPrefs);
+    }
+
     boolean getScreensaverNightModeOn() {
         return SettingsDAO.getScreensaverNightModeOn(mPrefs);
+    }
+
+    boolean getScreensaverShowAmPmOn() {
+        return SettingsDAO.getScreensaverShowAmPmOn(mPrefs);
+    }
+
+    boolean getScreensaverBoldTextOn() {
+        return SettingsDAO.getScreensaverBoldTextOn(mPrefs);
     }
 
     boolean getShowHomeClock() {

src/com/android/deskclock/settings/ScreensaverSettingsActivity.java
@@ -19,10 +19,11 @@ package com.android.deskclock.settings;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import androidx.preference.CheckBoxPreference;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
+import androidx.preference.SeekBarPreference;
 
 import com.android.deskclock.R;
 import com.android.deskclock.data.DataModel;
@@ -34,7 +35,14 @@ import com.android.deskclock.widget.CollapsingToolbarBaseActivity;
 public final class ScreensaverSettingsActivity extends CollapsingToolbarBaseActivity {
 
     public static final String KEY_CLOCK_STYLE = "screensaver_clock_style";
+    public static final String KEY_CLOCK_COLOR = "screensaver_clock_color";
     public static final String KEY_NIGHT_MODE = "screensaver_night_mode";
+    public static final String KEY_NIGHT_MODE_COLOR = "screensaver_clock_night_mode_color";
+    public static final String KEY_NIGHT_MODE_DND = "screensaver_clock_night_mode_dnd";
+    public static final String KEY_NIGHT_MODE_BRIGHTNESS =
+            "screensaver_clock_night_mode_brightness";
+    public static final String KEY_SHOW_AMPM = "screensaver_show_ampm";
+    public static final String KEY_BOLD_TEXT = "screensaver_bold_text";
     private static final String PREFS_FRAGMENT_TAG = "prefs_fragment";
 
     @Override
@@ -62,6 +70,7 @@ public final class ScreensaverSettingsActivity extends CollapsingToolbarBaseActi
     public static class PrefsFragment extends PreferenceFragmentCompat
             implements Preference.OnPreferenceChangeListener {
 
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -82,24 +91,79 @@ public final class ScreensaverSettingsActivity extends CollapsingToolbarBaseActi
 
         @Override
         public boolean onPreferenceChange(Preference pref, Object newValue) {
-            if (KEY_CLOCK_STYLE.equals(pref.getKey())) {
-                final ListPreference clockStylePref = (ListPreference) pref;
-                final int index = clockStylePref.findIndexOfValue((String) newValue);
-                clockStylePref.setSummary(clockStylePref.getEntries()[index]);
+            switch (pref.getKey()) {
+                case KEY_CLOCK_STYLE:
+                    final ListPreference clockStylePref = (ListPreference) pref;
+                    final int clockStyleindex = clockStylePref.findIndexOfValue((String) newValue);
+                    clockStylePref.setSummary(clockStylePref.getEntries()[clockStyleindex]);
+                    break;
+                case KEY_NIGHT_MODE_COLOR:
+                case KEY_CLOCK_COLOR:
+                    final ListPreference clockColorPref = (ListPreference) pref;
+                    final int clockColorindex = clockColorPref.findIndexOfValue((String) newValue);
+                    clockColorPref.setSummary(clockColorPref.getEntries()[clockColorindex]);
+                    break;
+                case KEY_NIGHT_MODE_BRIGHTNESS:
+                    final SeekBarPreference clockBrightness = (SeekBarPreference) pref;
+                    final String progress = getResources().getString(
+                            R.string.clock_brightness_percentage, String.valueOf(newValue) );
+                    clockBrightness.setSummary(progress);
+                    break;
             }
             return true;
         }
 
         private void refresh() {
             final ListPreference clockStylePref = findPreference(KEY_CLOCK_STYLE);
-            final CheckBoxPreference nightModePref = findPreference(KEY_NIGHT_MODE);
-            if (clockStylePref != null && nightModePref != null) {
+            final ListPreference clockColorPref = findPreference(KEY_CLOCK_COLOR);
+            final ListPreference nightModeColorPref = findPreference(KEY_NIGHT_MODE_COLOR);
+            final SwitchPreferenceCompat nightModePref = findPreference(KEY_NIGHT_MODE);
+            final SwitchPreferenceCompat nightModeDndPref = findPreference(KEY_NIGHT_MODE_DND);
+            final SwitchPreferenceCompat showAmPmPref = findPreference(KEY_SHOW_AMPM);
+            final SwitchPreferenceCompat boldTextPref = findPreference(KEY_BOLD_TEXT);
+            final SeekBarPreference nightModeBrightness = findPreference(KEY_NIGHT_MODE_BRIGHTNESS);
+            if (clockStylePref != null) {
                 final int index = clockStylePref.findIndexOfValue(DataModel.getDataModel().
                         getScreensaverClockStyle().toString().toLowerCase());
                 clockStylePref.setValueIndex(index);
                 clockStylePref.setSummary(clockStylePref.getEntries()[index]);
                 clockStylePref.setOnPreferenceChangeListener(this);
+            }
+            if (clockColorPref != null) {
+                final int indexColor = clockColorPref.findIndexOfValue(DataModel.getDataModel().
+                        getScreensaverClockColor());
+                clockColorPref.setValueIndex(indexColor);
+                clockColorPref.setSummary(clockColorPref.getEntries()[indexColor]);
+                clockColorPref.setOnPreferenceChangeListener(this);
+            }
+            if (nightModeColorPref != null) {
+                final int indexColor = nightModeColorPref.findIndexOfValue(DataModel.getDataModel().
+                        getScreensaverClockNightModeColor());
+                nightModeColorPref.setValueIndex(indexColor);
+                nightModeColorPref.setSummary(clockColorPref.getEntries()[indexColor]);
+                nightModeColorPref.setOnPreferenceChangeListener(this);
+            }
+            if (nightModePref != null) {
                 nightModePref.setChecked(DataModel.getDataModel().getScreensaverNightModeOn());
+            }
+            if (nightModeDndPref != null) {
+                nightModeDndPref.setChecked(DataModel.getDataModel()
+                        .getScreensaverNightModeDndOn());
+            }
+            if (showAmPmPref != null) {
+                showAmPmPref.setChecked(DataModel.getDataModel().getScreensaverShowAmPmOn());
+            }
+            if (boldTextPref != null) {
+                boldTextPref.setChecked(DataModel.getDataModel().getScreensaverBoldTextOn());
+            }
+            if (nightModeBrightness != null) {
+                final int percentage = DataModel.getDataModel().getScreensaverNightModeBrightness();
+                nightModeBrightness.setValue(percentage);
+                final String progress = getResources().getString(
+                        R.string.clock_brightness_percentage, String.valueOf(percentage));
+                nightModeBrightness.setSummary(progress);
+                nightModeBrightness.setOnPreferenceChangeListener(this);
+                nightModeBrightness.setUpdatesContinuously(true);
             }
         }
     }